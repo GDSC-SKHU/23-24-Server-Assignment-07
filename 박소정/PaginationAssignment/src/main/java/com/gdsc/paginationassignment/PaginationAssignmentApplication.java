44e91767c58244d81832caaf3057d235ee95c3f5
Park, Sojeong <sojeong2101@gmail.com>
2023-12-02 19:37:05 +0900

    docs: 구현 기능 정리

---
+++ 박소정/PaginationAssignment/src/main/java/com/gdsc/paginationassignment/PaginationAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.paginationassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationAssignmentApplication.class, args);
+	}
+
+}
---
+++ 박소정/PaginationAssignment/src/test/java/com/gdsc/paginationassignment/PaginationAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.paginationassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
